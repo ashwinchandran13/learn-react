2b8d36719ec6a3049abe4a2139d7cb7e44e487dc
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-04-05 19:31:13 +0530

    basic jsx

--- first-react/index.js
+++ first-react/index.js
@@ -1,1 +1,33 @@
-ReactDOM.render(<ul><li>dog</li><li>cat</li><li>mouse</li></ul>, document.getElementById("root"))
+// const MainContent = () => {
+//     return <h1>I'm learning react</h1>;
+// }
+
+// const element = (
+//     <div>
+//         <h1 className="header">Hello, react!</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// );
+
+// console.log('element', element);
+
+const navBar = (
+    <nav>
+        <h1>Learn React</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+);
+
+ReactDOM.render(
+    navBar, 
+    document.getElementById("root")
+);
+
+// const h1 = document.createElement("h1");
+//  h1.textContent = "This is an imperative way to program";
+//  h1.className = "header";
+//  document.getElementById("root").append(h1);
