5529b976085b46978db045873ee7ecaeca79e443
Ashwin Chandran <ashwinchandran13@gmail.com>
2022-07-03 00:07:32 +0530

    feat: travel-journal started

---
+++ travel-journal/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={'images/globe.png'} alt='globe symbol' height={24} width={24} />
+        <p>my travel journal</p>
+    </nav>
+  )
+}
+
+export default Navbar
