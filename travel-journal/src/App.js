d1f600cdffcdba52b3c974921436619ec41542b6
Ashwin Chandran <ashwin.chandran@neoito.com>
2022-07-06 16:29:07 +0530

    project completion

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,9 +1,13 @@
+import Card from "./components/Card";
 import Navbar from "./components/Navbar";
+import data from './data.js';
 
 function App() {
+  const cardComponent = data.map((item) => <Card key={item.title} {...item}/>); 
   return (
     <div className="App">
       <Navbar />
+      {cardComponent}
     </div>
   );
 }
--- travel-journal/src/components/Card.jsx
+++ travel-journal/src/components/Card.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({description, endDate, googleMapsUrl, imageUrl, location, startDate, title}) => {
   return (
-    <div></div>
+    <div className='card'>
+      <img src={imageUrl} className='card--location-pic' alt="pic" />
+      <div className='card--details'>
+        <div className='card--location'>
+          <img src={'images/map.png'} alt="map pointer" className='location--pointer' />
+          <span className='location--name'>{location}</span>
+          <a href={googleMapsUrl} alt='location'>View on Google Maps</a>
+        </div>
+        <h1>{title}</h1>
+        <div className='card--duration'>
+          <span>{startDate} - {endDate}</span>
+        </div>
+        <p>{description}</p>
+      </div>
+    </div>
   )
 }
 
