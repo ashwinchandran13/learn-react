d9eedd449074f2cbcb52ce7929adde61ed3c1540
Ashwin Chandran <ashwin.chandran@neoito.com>
2022-07-27 21:04:07 +0530

    some changes

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -4,27 +4,8 @@ import Meme from './component/Meme';
 
 const App = () => {
 
-  function greeting(name) {
-    let greetings = '';
-    const date = new Date();
-    const hours =11;
-    console.log({hours});
-    if (hours > 11 && hours < 17) {
-      greetings = 'afternoon';
-    } else if (hours > 3 && hours < 12) {
-      greetings = 'morning';
-    } else if (hours > 17 && hours < 20) {
-      greetings = 'evening'
-    } else {
-      greetings = 'night'
-    }
-
-    console.log(`Good ${greetings}, ${name}`);
-  }
-
   return (
     <div>
-      <button onClick={() => greeting('alan')}>click</button>
       <Header />
       <Meme />
     </div>
--- meme-generator/src/component/Meme.jsx
+++ meme-generator/src/component/Meme.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../memesData';
 
 const Meme = () => {
 
-    function handleClick () {
-      const totalMeme = data.length;
-      console.log(data[Math.floor(Math.random() * totalMeme)].url);
-    }
+  const [memeImage, setMemeImage] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'https://imgflip.com/s/meme/Drake-Hotline-Bling.jpg'
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(data);
+
+  function getMemeImage(url) {
+    setMemeImage(prevImage => {
+      return {
+        ...prevImage,
+        randomImage: url
+      }
+    });
+  }
+
+  function handleClick () {
+    const totalMeme = allMemeImages.length;
+    getMemeImage(data[Math.floor(Math.random() * totalMeme)].url);
+  }
+
     
   return (
     <main>
-        <img src={'images/troll.png'} alt='troll' onMouseEnter={handleClick}/>
-        <div>
+        <div className='form--container'>
             <input type="text" placeholder='Top Text' name='topText' className='form--inputField' />
             <input type="text" placeholder='Bottom Text' name='bottomText' className='form--inputField' />
             <button className='form--submitBtn' onClick={handleClick}>Get a new meme image  🖼</button>
+            <img className='form--meme-image' src={memeImage.randomImage} alt={memeImage.randomImage} />
         </div>
     </main>
   )
